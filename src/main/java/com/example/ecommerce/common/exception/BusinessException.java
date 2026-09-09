090fcc61fd2b69b40b7d1ed6d1487c7dba3b2b0b
Yusuf GUNES <yusufguness1.00@gmail.com>
2026-09-10 00:02:37 +0300

    feat/exception add exceptions errors, add Getter in BusinessException.

--- src/main/java/com/example/ecommerce/common/exception/BusinessException.java
+++ src/main/java/com/example/ecommerce/common/exception/BusinessException.java
@@ -1,5 +1,8 @@
 package com.example.ecommerce.common.exception;
 
+import lombok.Getter;
+
+@Getter
 public class BusinessException extends  RuntimeException {
 
     private final ErrorCode errorCode;
