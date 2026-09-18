8fd3c0fbf713435181807305aabdc90fc5e978bb
Yusuf GUNES <yusufguness1.00@gmail.com>
2026-09-19 01:40:42 +0300

    feat: products controllers is added basically

---
+++ src/main/java/com/example/ecommerce/product/controller/AdminProductController.java
@@ -0,0 +1,11 @@
+package com.example.ecommerce.product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/api/v1/admin/products")
+@RequiredArgsConstructor
+public class AdminProductController {
+}
