ac85829aea4797ccdcd63c48caeba79d098a100b
Yusuf GUNES <yusufguness1.00@gmail.com>
2026-09-23 19:02:00 +0300

    feat(cart): add controller and service.

--- src/main/java/com/example/ecommerce/cart/controller/CartController.java
+++ src/main/java/com/example/ecommerce/cart/controller/CartController.java
@@ -1,15 +1,16 @@
 package com.example.ecommerce.cart.controller;
 
+import com.example.ecommerce.cart.dto.request.AddCartItemRequest;
+import com.example.ecommerce.cart.dto.request.UpdateCartItemRequest;
 import com.example.ecommerce.cart.dto.response.CartResponse;
 import com.example.ecommerce.cart.service.CartService;
 import com.example.ecommerce.user.entity.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +26,46 @@ public class CartController {
     )
     {
         return ResponseEntity.ok( cartService.getMyCart(userDetails.getId())  );
+    }
+
+    @PostMapping
+    @Operation(summary = "Add product variant to current user's cart")
+    public ResponseEntity<Void> addItem(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @Valid @RequestBody AddCartItemRequest request
+    ) {
+        cartService.addItem(userDetails.getId(), request);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping
+    @Operation(summary = "Update cart item quantity")
+    public ResponseEntity<Void> updateItem(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long cartItemId,
+            @Valid @RequestBody UpdateCartItemRequest request
+    ) {
+        cartService.updateItem(
+                userDetails.getId(),
+                cartItemId,
+                request
+        );
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Remove item from current user's cart")
+    public ResponseEntity<Void> deleteItem(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
+            @PathVariable Long cartItemId
+    ) {
+        cartService.deleteItem(
+                userDetails.getId(),
+                cartItemId
+        );
+
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/ecommerce/cart/service/CartService.java
+++ src/main/java/com/example/ecommerce/cart/service/CartService.java
@@ -1,21 +1,30 @@
 package com.example.ecommerce.cart.service;
 
+import com.example.ecommerce.cart.dto.request.AddCartItemRequest;
+import com.example.ecommerce.cart.dto.request.UpdateCartItemRequest;
 import com.example.ecommerce.cart.dto.response.CartItemResponse;
 import com.example.ecommerce.cart.dto.response.CartResponse;
 import com.example.ecommerce.cart.entity.Cart;
+import com.example.ecommerce.cart.entity.CartItem;
 import com.example.ecommerce.cart.repository.CartItemRepository;
 import com.example.ecommerce.cart.repository.CartRepository;
+import com.example.ecommerce.cart.repository.view.CartItemSummary;
 import com.example.ecommerce.common.exception.BusinessException;
 import com.example.ecommerce.common.exception.ErrorCode;
-import com.example.ecommerce.user.entity.User;
+
+import com.example.ecommerce.product.dto.response.FindVariantForCart;
+import com.example.ecommerce.product.dto.response.integrationCart.CartProductVariant;
+import com.example.ecommerce.product.service.ProductVariantService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
+
 
 @Service
 @RequiredArgsConstructor
@@ -25,6 +34,7 @@ public class CartService {
 
     private final CartRepository cartRepository;
     private final CartItemRepository cartItemRepository;
+    private final ProductVariantService productVariantService;
 
     @Transactional
     public Cart createCart(Long userId) {
@@ -37,29 +47,174 @@ public class CartService {
         return cartRepository.save(cart);
     }
 
+
     public CartResponse getMyCart(Long userId) {
 
         log.debug("Fetching cart. userId={}", userId);
 
         Cart cart = cartRepository.findByUserId(userId)
                 .orElseThrow(() ->
-                        new BusinessException(ErrorCode.CART_NOT_FOUND)       );
+                        new BusinessException(ErrorCode.CART_NOT_FOUND)
+                );
 
-        List<CartItemResponse> items = cartItemRepository.findCartItems(cart.getId());
+        List<CartItemSummary> cartItems =
+                cartItemRepository.findCartItemSummaries(cart.getId());
+
+        if (cartItems.isEmpty()) {
+            return new CartResponse(
+                    List.of(),
+                    BigDecimal.ZERO
+            );
+        }
+
+        List<Long> variantIds = cartItems.stream()
+                .map(CartItemSummary::getProductVariantId)
+                .toList();
+
+        Map<Long, CartProductVariant> productVariants =
+                productVariantService.findCartProductVariants(variantIds);
+
+        List<CartItemResponse> items = cartItems.stream()
+                .map(cartItem -> {
+
+                    CartProductVariant productVariant =
+                            productVariants.get(
+                                    cartItem.getProductVariantId()
+                            );
+
+                    return new CartItemResponse(
+                            cartItem.getId(),
+                            cartItem.getProductVariantId(),
+                            productVariant.productName(),
+                            productVariant.primaryImageUrl(),
+                            productVariant.color(),
+                            productVariant.size(),
+                            productVariant.price(),
+                            cartItem.getQuantity()
+                    );
+                })
+                .toList();
 
         BigDecimal totalPrice = getTotalPrice(items);
 
-        log.debug("Fetched cart. userId={}, itemCount={}", userId,  items.size()     );
+        log.debug("Fetched cart. userId={}, itemCount={}",
+                userId,
+                items.size()     );
 
         return new CartResponse(items, totalPrice);
     }
 
     @Transactional
+    public void addItem(Long userId, @NonNull AddCartItemRequest request) {
+
+        log.debug("Adding item to cart. userId={}, productVariantId={}, quantity={}",
+                userId,
+                request.productVariantId(),
+                request.quantity()
+        );
+
+        Cart cart = cartRepository.findByUserId(userId)
+                .orElseThrow(() ->
+                        new BusinessException(ErrorCode.CART_NOT_FOUND)  );
+
+        FindVariantForCart variant =
+                productVariantService.getVariantInfo(
+                        request.productVariantId()     );
+
+        validateVariant(variant);
+
+        CartItem cartItem = cartItemRepository
+                .findByCartIdAndProductVariantId(
+                        cart.getId(),
+                        variant.id()
+                )
+                .orElse(null);
+
+        int newQuantity = calculateNewQuantity( cartItem, request.quantity()  );
+
+        validateStock(variant, newQuantity);
+
+        if (cartItem != null) {
+            cartItem.setQuantity(newQuantity);
+        } else {
+            cartItem = new CartItem(
+                    cart,
+                    variant.id(),
+                    newQuantity      );
+            cartItemRepository.save(cartItem);
+        }
+
+        log.info("Cart item added. userId={}, productVariantId={}, quantity={}",
+                userId,
+                variant.id(),
+                newQuantity    );
+
+    }
+
+    @Transactional
+    public void updateItem(
+            Long userId,
+            Long cartItemId,
+            @NonNull UpdateCartItemRequest request
+    ) {
+
+        log.debug("Updating cart item. userId={}, cartItemId={}, quantity={}",
+                userId,
+                cartItemId,
+                request.quantity()
+        );
+
+        CartItem cartItem = cartItemRepository
+                .findByIdAndCartUserId(cartItemId, userId)
+                .orElseThrow(() ->
+                        new BusinessException(ErrorCode.CART_ITEM_NOT_FOUND)
+                );
+
+        FindVariantForCart variant =
+                productVariantService.getVariantInfo(
+                        cartItem.getProductVariantId()
+                );
+
+        validateVariant(variant);
+        validateStock(variant, request.quantity());
+
+        cartItem.setQuantity(request.quantity());
+
+        log.info(
+                "Cart item updated. userId={}, cartItemId={}, quantity={}",
+                userId,
+                cartItemId,
+                request.quantity()
+        );
+    }
 
 
+    @Transactional
+    public void deleteItem(Long userId, Long cartItemId) {
 
-    // helper
-    private BigDecimal getTotalPrice(List<CartItemResponse> items) {
+        log.debug("Deleting cart item. userId={}, cartItemId={}",
+                userId,
+                cartItemId
+        );
+
+        CartItem cartItem = cartItemRepository
+                .findByIdAndCartUserId(cartItemId, userId)
+                .orElseThrow(() ->
+                        new BusinessException(ErrorCode.CART_ITEM_NOT_FOUND)
+                );
+
+        cartItemRepository.delete(cartItem);
+
+        log.info("Cart item deleted. userId={}, cartItemId={}, productVariantId={}",
+                userId,
+                cartItemId,
+                cartItem.getProductVariantId()
+        );
+    }
+
+    // HELPERS
+
+    private BigDecimal getTotalPrice(@NonNull List<CartItemResponse> items) {
         return items.stream()
                 .map(item ->
                         item.price()
@@ -68,5 +223,34 @@ public class CartService {
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
+    private void validateVariant(FindVariantForCart variant) {
 
+        if (!variant.active()) {
+            throw new BusinessException(
+                    ErrorCode.PRODUCT_VARIANT_NOT_FOUND
+            );
+        }
+    }
+
+    private int calculateNewQuantity(
+            CartItem cartItem,
+            int requestedQuantity
+    ) {
+        if (cartItem == null) {
+            return requestedQuantity;
+        }
+
+        return cartItem.getQuantity() + requestedQuantity;
+    }
+
+    private void validateStock(
+            FindVariantForCart variant,
+            int quantity
+    ) {
+        if (variant.stockQuantity() < quantity) {
+            throw new BusinessException(
+                    ErrorCode.INSUFFICIENT_STOCK
+            );
+        }
+    }
 }
